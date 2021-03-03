981eacb5c24f25a4cd8a90a41c230f052c9ec082
Turnbaugh OT2 <turnbaughlab@Marsha.local>
2021-03-02 17:42:16 -0800

    Update 1_ZymoMagWash.py

--- ot2_scripts/1_ZymoMagWash.py
+++ ot2_scripts/1_ZymoMagWash.py
@@ -25,7 +25,7 @@ def run(protocol: protocol_api.ProtocolContext):
 	elution_to_plate = 40 # ul to transfer to final elution plate
 	capture_depth = -3 # depth below ideal bottom of plate to remove supernatants, this may be required as a function of a poor calibration or labware def
 	capture_min = 2 # number of minutes to capture beads on magnets
-	cols_to_extract = [1, 2] # which columns should be extracted?
+	cols_to_extract = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] # which columns should be extracted?
 	nmix = 5 # number of times to pipette to mix
 	drying_min = 20 # number of minutes to evaporate residual ethanol !!!! SET TO 20
 	trash_speed = 1 # the relative speed to discard of liquids into trash, this is an integer multiplier of normal speed, set higher to clear bubbles on outside of tip
@@ -54,23 +54,14 @@ def run(protocol: protocol_api.ProtocolContext):
 	set_rail_lights = True
 
 	# define pipettes
-	multichannel = protocol.load_instrument('p300_multi_gen2', 'right', tip_racks=[tips_binding, tips_wash1, tips_wash2, tips_wash2_repeat, tips_elution])
+	multichannel = protocol.load_instrument('p300_multi_gen2', 'left', tip_racks=[tips_binding, tips_wash1, tips_wash2, tips_wash2_repeat, tips_elution])
 	#P1000 = protocol.load_instrument('p1000_single_gen2', 'right', tip_racks=[tips_binding, tips_wash1, tips_wash2, tips_wash2rep])
 
 	### Prerun setup ########################################
 	MagModule.disengage()
 
 	### MAG BINDING ######################################
-	if AddBeads:
-		protocol.comment('--------->Adding 25ul mag beads')
-		multichannel.pick_up_tip(tips_binding['A1'])
-		multichannel.mix(10, 100, BeadsAndWater['A1'].bottom(3)) # mix beads 20 x by pulling up 100ul
-		for i in cols_to_extract:
-			multichannel.aspirate(25, BeadsAndWater['A1'])
-			multichannel.dispense(25, BindingPlate['A'+str(i)].top(0))
-			multichannel.blow_out()
-		multichannel.return_tip()
-		
+
 	if AddBindingBuffer:
 		protocol.comment('--------->Adding 600 ul binding buffer')
 		multichannel.pick_up_tip(tips_binding['A1'])
@@ -81,13 +72,22 @@ def run(protocol: protocol_api.ProtocolContext):
 			multichannel.aspirate(200, BindingBuffer['A1'])
 			multichannel.dispense(200, BindingPlate['A'+str(i)].top(4))
 			#multichannel.blow_out()
-		multichannel.return_tip()	
+			multichannel.aspirate(200, BindingBuffer['A1'])
+			multichannel.dispense(200, BindingPlate['A'+str(i)].top(4))
+			#multichannel.blow_out()
+		multichannel.return_tip()
+		
+	if AddBeads:
+		protocol.comment('--------->Adding 25ul mag beads')
+		multichannel.pick_up_tip(tips_binding['A2'])
+		multichannel.mix(10, 100, BeadsAndWater['A1'].bottom(3)) # mix beads 20 x by pulling up 100ul
+		for i in cols_to_extract:
+			multichannel.aspirate(25, BeadsAndWater['A1'])
+			multichannel.dispense(25, BindingPlate['A'+str(i)].top(4)
+		multichannel.return_tip()
 		for i in cols_to_extract:
 			multichannel.pick_up_tip(tips_binding['A'+str(i)])
-			multichannel.aspirate(200, BindingBuffer['A1'])
-			multichannel.dispense(200, BindingPlate['A'+str(i)])
-			multichannel.mix(nmix, 180, BindingPlate['A'+str(i)].bottom(5))
-			multichannel.blow_out()
+			multichannel.mix(10, 200, BeadsAndWater['A1'].bottom(3))
 			multichannel.return_tip()
 		
 	if CaptureBinding:
@@ -107,7 +107,7 @@ def run(protocol: protocol_api.ProtocolContext):
 			multichannel.aspirate(200, BindingPlate['A'+str(i)])
 			multichannel.dispense(200, fixed_trash, rate = trash_speed)
 			#multichannel.blow_out()
-			multichannel.aspirate(200, BindingPlate['A'+str(i)].bottom(capture_depth)) # is this a calibration issue?
+			multichannel.aspirate(200, BindingPlate['A'+str(i)].bottom(capture_depth), rate=0.2)
 			multichannel.dispense(200, fixed_trash, rate = trash_speed)
 			#multichannel.blow_out()
 			multichannel.return_tip()
@@ -134,7 +134,7 @@ def run(protocol: protocol_api.ProtocolContext):
 			multichannel.pick_up_tip(tips_wash1['A'+str(i)])
 			multichannel.aspirate(100, MagWash1['A1'])
 			multichannel.dispense(100, BindingPlate['A'+str(i)])
-			multichannel.mix(nmix, 180, BindingPlate['A'+str(i)].bottom(5))
+			multichannel.mix(nmix, 180, BindingPlate['A'+str(i)].bottom(2))
 			multichannel.blow_out()
 			multichannel.return_tip()
 		MagModule.engage()
@@ -153,7 +153,7 @@ def run(protocol: protocol_api.ProtocolContext):
 			multichannel.aspirate(200, BindingPlate['A'+str(i)])
 			multichannel.dispense(200, fixed_trash, rate = trash_speed)
 			#multichannel.blow_out()
-			multichannel.aspirate(200, BindingPlate['A'+str(i)].bottom(capture_depth))
+			multichannel.aspirate(200, BindingPlate['A'+str(i)].bottom(capture_depth), rate=0.2)
 			multichannel.dispense(200, fixed_trash, rate = trash_speed)
 			multichannel.blow_out()
 			multichannel.return_tip()
@@ -180,7 +180,7 @@ def run(protocol: protocol_api.ProtocolContext):
 			multichannel.pick_up_tip(tips_wash2['A'+str(i)])
 			multichannel.aspirate(100, MagWash2['A1'])
 			multichannel.dispense(100, BindingPlate['A'+str(i)])
-			multichannel.mix(nmix, 180, BindingPlate['A'+str(i)].bottom(5))
+			multichannel.mix(nmix, 180, BindingPlate['A'+str(i)].bottom(2))
 			multichannel.blow_out()
 			multichannel.return_tip()
 		MagModule.engage()
@@ -199,7 +199,7 @@ def run(protocol: protocol_api.ProtocolContext):
 			multichannel.aspirate(200, BindingPlate['A'+str(i)]) # is this a calibration issue?
 			multichannel.dispense(200, fixed_trash, rate = trash_speed)
 			#multichannel.blow_out()
-			multichannel.aspirate(200, BindingPlate['A'+str(i)].bottom(capture_depth))
+			multichannel.aspirate(200, BindingPlate['A'+str(i)].bottom(capture_depth), rate=0.2)
 			multichannel.dispense(200, fixed_trash, rate = trash_speed)
 			#multichannel.blow_out()
 			multichannel.return_tip()
@@ -245,7 +245,7 @@ def run(protocol: protocol_api.ProtocolContext):
 			multichannel.aspirate(200, BindingPlate['A'+str(i)]) # is this a calibration issue?
 			multichannel.dispense(200, fixed_trash, rate = trash_speed)
 			#multichannel.blow_out()
-			multichannel.aspirate(200, BindingPlate['A'+str(i)].bottom(capture_depth))
+			multichannel.aspirate(200, BindingPlate['A'+str(i)].bottom(capture_depth), rate=0.2)
 			multichannel.dispense(200, fixed_trash, rate = trash_speed)
 			#multichannel.blow_out()
 			multichannel.return_tip()
